68ab5fb40205dcb429d894c7bce7b3bb6fcd82e9
Leon520 <mqd3333826>
2016-12-21 01:04:45 +0800

    1、项目创建

---
+++ WinformMessage/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinformMessage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+        }
+
+        public override bool PreProcessMessage(ref Message msg)
+        {
+            Console.WriteLine(msg.Msg);
+            return base.PreProcessMessage(ref msg);
+        }
+    }
+}
